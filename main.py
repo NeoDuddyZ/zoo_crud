252dd763949e94fe9430c3d9b8277b4745fc6c01
Eduardo <creisos@protonmail.com>
2021-11-11 13:47:02 -0300

    First Refactoration

--- main.py
+++ main.py
@@ -1,8 +1,63 @@
 #need to be refactored
 from animal import Animal
-
-x = int()
 animal_list = []
+x = int()
+
+
+def create_animal():
+    name = str(input('Write the name of the animal: '))
+    type = str(input('Write the type of the animal: '))
+    new_animal = Animal(name, type)
+    animal_list.append(new_animal)
+
+def set_new_animal(index):
+    name = str(input('Write the new name of the animal: '))
+    type = str(input('Write the new type of the animal: '))
+    animal_list[index].update(name, type)
+
+def read_animal():
+    index = int(input("Write the index of the animal: (if you don't know type -1 to see all animals with index)\n"))
+    if index == -1 and len(animal_list) > 0:
+        print_animal_list()
+    elif index != -1 and len(animal_list) > 0 and index < len(animal_list):
+        print(animal_list[index].show_animal())
+    else:
+        pass
+
+def update_animal():
+    while(True):
+        index = int(input("Write the index of the animal: (if you don't know type -1 to see all animals with index or -2 to exit)\n"))
+        if index == -1 and len(animal_list) > 0:
+            print_animal_list()
+        elif index == -2 and len(animal_list) > 0:
+            break
+        elif index >= 0 and len(animal_list) > 0 and index < len(animal_list):
+            set_new_animal(index)
+            break
+        else:
+            pass
+
+def print_animal_list():
+        count = 0
+        for i in animal_list:
+            print(f'{count} - {i.show_animal()}')
+            count += 1
+
+def delete_animal():
+    while (True):
+            index = int(input(
+                "Write the index of the animal: (if you don't know type -1 to see all animals with index or -2 to exit)\n"))
+            if index == -1 and len(animal_list) > 0:
+                print_animal_list()
+            elif index == -2 and len(animal_list) > 0:
+                break
+            elif index >= 0 and len(animal_list) > 0 and index < len(animal_list):
+                animal_list.pop(index)
+                break
+            else:
+                pass
+
+
 while(True):
     x = int(input('1-Create animal\n'
           '2-Read Animal\n'
@@ -11,59 +66,15 @@ while(True):
           '5-List Animals\n'
           '0-Exit\n'))
     if x == 1:
-        name = str(input('Write the name of the animal: '))
-        type = str(input('Write the type of the animal: '))
-        new_animal = Animal(name, type)
-        animal_list.append(new_animal)
+        create_animal()
     elif x == 2:
-        index = int(input("Write the index of the animal: (if you don't know type -1 to see all animals with index)\n"))
-        if index == -1 and len(animal_list) > 0:
-            count = 0
-            for i in animal_list:
-                print(f'{count} - {i.show_animal()}')
-                count += 1
-        elif index != -1  and len(animal_list) > 0 and index < len(animal_list):
-            print(animal_list[index].show_animal())
-        else:
-            pass
+        read_animal()
     elif x == 3:
-        while(True):
-            index = int(input("Write the index of the animal: (if you don't know type -1 to see all animals with index or -2 to exit)\n"))
-            if index == -1 and len(animal_list) > 0:
-                count = 0
-                for i in animal_list:
-                    print(f'{count} - {i.show_animal()}')
-                    count += 1
-            elif index == -2 and len(animal_list) > 0:
-                break
-            elif index >= 0 and len(animal_list) > 0 and index < len(animal_list):
-                name = str(input('Write the new name of the animal: '))
-                type = str(input('Write the new type of the animal: '))
-                animal_list[index].update(name, type)
-                break
-            else:
-                pass
+        update_animal()
     elif x == 4:
-        while (True):
-            index = int(input(
-                "Write the index of the animal: (if you don't know type -1 to see all animals with index or -2 to exit)\n"))
-            if index == -1 and len(animal_list) > 0:
-                count = 0
-                for i in animal_list:
-                    print(f'{count} - {i.show_animal()}')
-                    count += 1
-            elif index == -2 and len(animal_list) > 0:
-                break
-            elif index >= 0 and len(animal_list) > 0 and index < len(animal_list):
-                animal_list.pop(index)
-                break
-            else:
-                pass
+        delete_animal()
     elif x == 5:
-        count = 0
-        for i in animal_list:
-            print(f'{count} - {i.show_animal()}')
-            count += 1
+        print_animal_list()
     elif x == 0:
         break
     else:
